77fba4549d6c29ebf2edf6496b5e4db467603b3f
shuvo8453 <saifulshuvo2@gmail.com>
2021-07-04 02:21:25 +0600

    updating add-admin.php

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -4,6 +4,56 @@
 
     <div class="wrapper">
         <h1>Manage Food</h1>
+
+        <br> <br>
+
+        <!-- Button to add admin -->
+        <a href="#" class="btn-primary">Add Food</a>
+
+        <br> <br> <br>
+
+        <table class="tbl-full">
+
+            <tr>
+                <th>S.N</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Shuvo</td>
+                <td>Shuvo</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>2</td>
+                <td>Shuvo</td>
+                <td>Shuvo</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>3</td>
+                <td>Shuvo</td>
+                <td>Shuvo</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+        </table>
+
+
     </div>  
     
 </div>
