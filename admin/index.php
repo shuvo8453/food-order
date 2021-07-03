8813f1be5cd2d27cd711559958e8192e1d352df5
shuvo8453 <saifulshuvo2@gmail.com>
2021-07-03 12:28:34 +0600

    developig food-order website

---
+++ admin/index.php
@@ -0,0 +1,49 @@
+<html>
+
+<head>
+    <title> Food order website: Home Page </title>
+
+    <!-- <link rel="stylesheet" href="../css/admin.css">-->
+    <link rel="stylesheet" href="admin.css">
+
+</head>
+
+<body>
+    <!-- form section start. -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Catagory</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- form section ends. -->
+
+    <!-- main content section start. -->
+    <div class="main-content">
+        <div class="wrapper">
+            <strong>DASHBOARD</strong>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                Catagories
+            </div>
+
+        </div>
+    </div>
+    <!-- main content section ends. -->
+
+    <!-- footer section start. -->
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2021 All rights reserved. Some Resturant. Delivered By : <a href="#"> Shuvo </a></p>
+        </div>
+    </div>
+    <!-- footer section ends. -->
+</body>
+
+</html>
